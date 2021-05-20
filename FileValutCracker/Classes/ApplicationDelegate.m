55efa8769e3362bd0423a651384627f8aeafe46f
krishpranav <krisna.pranav@gmail.com>
2021-05-20 11:24:00 +0530

    [+] Application Delegate functions [+]

--- FileValutCracker/Classes/ApplicationDelegate.m
+++ FileValutCracker/Classes/ApplicationDelegate.m
@@ -6,4 +6,53 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "ApplicationDelegate.h"
+#import "AboutWindowController.h"
+#import "MainWindowController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApplicationDelegate()
+
+@property( atomic, readwrite, strong, nullable ) AboutWindowController * aboutWindowController;
+@property( atomic, readwrite, strong, nullable ) MainWindowController  * mainWindowController;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+@implementation ApplicationDelegate
+
+- ( void )applicationDidFinishLaunching: ( NSNotification * )notification
+{
+    ( void )notification;
+    
+    self.mainWindowController = [ MainWindowController new ];
+    
+    [ self.mainWindowController.window center ];
+    [ self.mainWindowController.window makeKeyAndOrderFront: nil ];
+}
+
+- ( BOOL )applicationShouldTerminateAfterLastWindowClosed: ( NSApplication * )sender
+{
+    ( void )sender;
+    
+    return YES;
+}
+
+- ( IBAction )showAboutWindow: ( nullable id )sender
+{
+    @synchronized( self )
+    {
+        if( self.aboutWindowController == nil )
+        {
+            self.aboutWindowController = [ AboutWindowController new ];
+            
+            [ self.aboutWindowController.window center ];
+        }
+        
+        [ self.aboutWindowController.window makeKeyAndOrderFront: sender ];
+    }
+}
+
+@end
