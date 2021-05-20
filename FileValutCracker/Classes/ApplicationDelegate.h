2c00f1af7361da5904a68529eb3f160277f036f8
krishpranav <krisna.pranav@gmail.com>
2021-05-20 11:19:07 +0530

    [+] Application Delegate classes [+]

---
+++ FileValutCracker/Classes/ApplicationDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ApplicationDelegate.h
+//  FileValutCracker
+//
+//  Created by Elangovan Ayyasamy on 20/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+@import Cocoa;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApplicationDelegate: NSObject < NSApplicationDelegate >
+
+- ( IBAction )showAboutWindow: ( nullable id )sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
