9bc69548a1af477cc3f401ad394f56e870178c67
krishpranav <krisna.pranav@gmail.com>
2021-05-20 11:09:04 +0530

    [+] Adding new files [+}

---
+++ FileValutCracker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FileValutCracker
+//
+//  Created by Elangovan Ayyasamy on 20/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FileValutCracker/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  FileValutCracker
+//
+//  Created by Elangovan Ayyasamy on 20/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
