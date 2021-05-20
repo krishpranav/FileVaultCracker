8b1e5b9e0826e5e7d1ff5d4eae462ccfb0d6b99e
krishpranav <krisna.pranav@gmail.com>
2021-05-20 12:24:56 +0530

    [+] Disk Management for cracking [+]

--- FileValutCracker/Classes/Private Headers/DiskManagement.h
+++ FileValutCracker/Classes/Private Headers/DiskManagement.h
@@ -6,8 +6,60 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#ifndef DiskManagement_h
-#define DiskManagement_h
+@import Foundation;
+@import DiskArbitration;
 
+NS_ASSUME_NONNULL_BEGIN
 
-#endif /* DiskManagement_h */
+@protocol DMManagerDelegate< NSObject >
+
+@optional
+
+- ( void )dmInterruptibilityChanged: ( BOOL )value;
+- ( void )dmAsyncFinishedForDisk: ( DADiskRef )disk mainError: ( int )mainError detailError: ( int )detailError dictionary: ( NSDictionary * )dictionary;
+- ( void )dmAsyncMessageForDisk: ( DADiskRef )disk string: ( NSString * )str dictionary: ( NSDictionary * )dict;
+- ( void )dmAsyncProgressForDisk: ( DADiskRef )disk barberPole: ( BOOL )barberPole percent: ( float )percent;
+- ( void )dmAsyncStartedForDisk: ( DADiskRef )disk;
+
+@end
+
+@protocol DMManagerClientDelegate< NSObject >
+
+@end
+
+@interface DMManager: NSObject
+
+@property( atomic, readwrite, weak, nullable   ) id< DMManagerDelegate       > delegate;
+@property( atomic, readwrite, weak, nullable   ) id< DMManagerClientDelegate > clientDelegate;
+@property( atomic, readonly                    ) BOOL                          checkClientDelegate;
+@property( atomic, readonly, nullable          ) NSArray                     * topLevelDisks;
+@property( atomic, readonly, nullable          ) NSArray                     * disks;
+@property( atomic, readwrite, assign, nullable ) DASessionRef                  defaultDASession;
+@property( atomic, readwrite, strong, nullable ) NSString                    * language;
+
++ ( instancetype )sharedManager;
++ ( instancetype )sharedManagerForThread;
+
+- ( BOOL )isCoreStoragePhysicalVolumeDisk: ( DADiskRef )disk error: ( NSError * __autoreleasing * )error;
+- ( BOOL )isCoreStorageLogicalVolumeDisk: ( DADiskRef )disk error: ( NSError * __autoreleasing * )error;
+- ( NSString * )diskUUIDForDisk: ( DADiskRef )disk error: ( NSError * __autoreleasing * )error;
+
+@end
+
+@interface DMCoreStorage: NSObject
+
+- ( instancetype )initWithManager: ( DMManager * )manager;
+
+- ( int )unlockLogicalVolume: ( NSString * )volumeUID options: ( NSDictionary * )options;
+- ( int )doCallDaemonForCoreStorage: ( NSString * )selector inputDict: ( NSMutableDictionary * )inputDict outputDict: ( NSDictionary  * _Nullable * _Nullable )outputDict checkDelegate: ( BOOL )checkDelegate sync: ( BOOL )sync;
+
+- ( int )logicalVolumeGroups: ( NSArray< NSString * > * _Nullable * _Nullable )groups;
+- ( int )logicalVolumeForDisk: ( DADiskRef )disk logicalVolume: ( NSString * _Nullable * _Nullable )logicalVolume;
+- ( int )physicalVolumeAndLogicalVolumeGroupForDisk:( DADiskRef )disk physicalVolume: ( NSString * _Nullable * _Nullable )physicalVolume logicalVolumeGroup: ( NSString * _Nullable * _Nullable )logicalVolumeGroup;
+- ( int )logicalVolumeGroupForLogicalVolume: ( NSString * )uuid logicalVolumeGroup:( NSString * _Nullable * _Nullable )group;
+- ( int )copyDiskForLogicalVolume: ( NSString * )uuid disk: ( DADiskRef _Nullable * _Nullable )disk;
+- ( int )isEncryptedDiskForLogicalVolume: ( NSString * )uuid encrypted:( BOOL * _Nullable )encrypted locked: ( BOOL * _Nullable )locked type: ( id _Nullable * _Nullable )type;
+
+@end
+
+NS_ASSUME_NONNULL_END
