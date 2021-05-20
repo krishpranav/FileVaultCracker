eb53c44e5320759b266c3e42be6c18d0e1c04e15
krishpranav <krisna.pranav@gmail.com>
2021-05-20 11:25:08 +0530

    [+] Added New Files [+]

---
+++ FileValutCracker/Classes/CoreStorageHelper.h
@@ -0,0 +1,13 @@
+//
+//  CoreStorageHelper.h
+//  FileValutCracker
+//
+//  Created by Elangovan Ayyasamy on 20/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef CoreStorageHelper_h
+#define CoreStorageHelper_h
+
+
+#endif /* CoreStorageHelper_h */
---
+++ FileValutCracker/Classes/CoreStorageHelper.m
@@ -0,0 +1,9 @@
+//
+//  CoreStorageHelper.m
+//  FileValutCracker
+//
+//  Created by Elangovan Ayyasamy on 20/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
