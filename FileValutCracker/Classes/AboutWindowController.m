38cdc8ccb2c7f0826886524e8e253ce66eb9b579
krishpranav <krisna.pranav@gmail.com>
2021-05-20 11:16:21 +0530

    [+] added instance type, void functions [+]

--- FileValutCracker/Classes/AboutWindowController.m
+++ FileValutCracker/Classes/AboutWindowController.m
@@ -6,16 +6,38 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
+
 #import "AboutWindowController.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface AboutWindowController()
 
-@property( atomic, readwrite, strong) NSString * name;
-@property( atomic, readwrite, strong) NSString * version;
-@property( atomic, readwrite, strong) NSString * copyright;
+@property( atomic, readwrite, strong ) NSString * name;
+@property( atomic, readwrite, strong ) NSString * version;
+@property( atomic, readwrite, strong ) NSString * copyright;
 
 @end
 
 NS_ASSUME_NONNULL_END
+
+@implementation AboutWindowController
+
+- ( instancetype )init
+{
+    return [ self initWithWindowNibName: NSStringFromClass( self.class ) ];
+}
+
+- ( void )windowDidLoad
+{
+    [ super windowDidLoad ];
+    
+    self.window.titlebarAppearsTransparent = YES;
+    self.window.titleVisibility            = NSWindowTitleHidden;
+    
+    self.name      = [ [ NSBundle mainBundle ] objectForInfoDictionaryKey: @"CFBundleName" ];
+    self.version   = [ [ NSBundle mainBundle ] objectForInfoDictionaryKey: @"CFBundleShortVersionString" ];
+    self.copyright = [ [ NSBundle mainBundle ] objectForInfoDictionaryKey: @"NSHumanReadableCopyright" ];
+}
+
+@end
