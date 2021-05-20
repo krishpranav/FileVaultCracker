7452cd04dcde4602281b16702103731247e39752
krishpranav <krisna.pranav@gmail.com>
2021-05-20 11:12:20 +0530

    About Window Controller classes

---
+++ FileValutCracker/Classes/AboutWindowController.h
@@ -0,0 +1,17 @@
+//
+//  AboutWindowController.h
+//  FileValutCracker
+//
+//  Created by Elangovan Ayyasamy on 20/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+@import Cocoa;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AboutWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
