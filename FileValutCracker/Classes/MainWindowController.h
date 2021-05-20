41e6ddbf6275aac058277c35b931228fda005673
krishpranav <krisna.pranav@gmail.com>
2021-05-20 12:07:13 +0530

    [+] Added Main Window Controller [+]

---
+++ FileValutCracker/Classes/MainWindowController.h
@@ -0,0 +1,13 @@
+//
+//  MainWindowController.h
+//  FileValutCracker
+//
+//  Created by Elangovan Ayyasamy on 20/05/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef MainWindowController_h
+#define MainWindowController_h
+
+
+#endif /* MainWindowController_h */
