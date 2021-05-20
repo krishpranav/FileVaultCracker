1ff083959fe6808671572caf7de22cc2c092bb11
krishpranav <krisna.pranav@gmail.com>
2021-05-20 11:31:44 +0530

    [+] Added CoreStorageHelper Instance added, init instance type updated [+]

--- FileValutCracker/Classes/CoreStorageHelper.m
+++ FileValutCracker/Classes/CoreStorageHelper.m
@@ -6,4 +6,55 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import DiskArbitration;
+
+#import "CoreStorageHelper.h"
+#import "DiskManagement.h"
+#import <stdatomic.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CoreStorageHelper() < DMManagerDelegate, DMManagerClientDelegate >
+{
+    atomic_bool _unlocked;
+}
+
+@property( atomic, readwrite, strong ) DMManager     * manager;
+@property( atomic, readwrite, strong ) DMCoreStorage * cs;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+@implementation CoreStorageHelper
+
++ ( instancetype )sharedInstance
+{
+    static dispatch_once_t once;
+    static id              instance = nil;
+    
+    dispatch_once
+    (
+     &once,
+     ^( void )
+     {
+         instance = [ self new ];
+     }
+     );
+    
+    return instance;
+}
+
+- ( instancetype )init
+{
+    if( ( self = [ super init ] ) )
+    {
+        self.manager                = [ DMManager new ];
+        self.manager.language       = @"English";
+        self.manager.delegate       = self;
+        self.manager.clientDelegate = self;
+        self.cs                     = [ [ DMCoreStorage alloc ] initWithManager: self.manager ];
+    }
+    
+    return self;
+}
